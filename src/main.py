f09a826c28fef710b3925f47353c6f5b3827959d
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-05-22 20:50:09 +0200

    plot and display

--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
 
+SEED = 1337
 
 def estimate(n: int, seed:int = None) -> float:
-  np.random.seed(seed if seed else 1337)
+  np.random.seed(seed if seed else SEED)
   x = np.random.random(n)
   y = np.random.random(n)
   s = np.sqrt(x**2 + y**2)
   included = s[s <= 1] # points inside circle
   return 4 * len(included) / n
 
+def plot_estimates(n: int) -> None:
+  x = list(range(1, n))
+  y = [estimate(iterations) for iterations in tqdm.tqdm(x)]
+  print(f"Last π estimate {y[-1]}")
+  plt.plot(x,y)
+  plt.xlabel('iterations')
+  plt.ylabel('estimate')
+  plt.axhline(y=y[-1], color='r', label='last π estimate')
+  plt.axhline(y=np.pi, color='r', linestyle='--', label='π')
+  plt.legend(loc='best')
+  plt.show()
+
 def main():
-  print(estimate(100000000))
+  param_N = os.environ.get('ITER')
+  N = int(param_N) if param_N else 1000000
+  plot_estimates(N)
 
 if __name__ == "__main__":
   main()
